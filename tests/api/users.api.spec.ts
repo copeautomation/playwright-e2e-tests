eab69c840044eb8b74e527a63934d4df9d287d38
Periyanayagam Santhappan <copeautomation@gmail.com>
2025-10-06 18:45:26 +1100

    latest code

--- tests/api/users.api.spec.ts
+++ tests/api/users.api.spec.ts
@@ -2,6 +2,7 @@ import { test, expect, request } from "@playwright/test";
 import { log } from "../helpers/logger.js";
 import constants from "../../data/constants.json";
 import TestData from "../../data/test-data.js";
+import fileHelper from "../helpers/file-helper.js";
 
 test.describe("REST API Demo", () => {
     let envConfig = undefined;
@@ -26,6 +27,9 @@ test.describe("REST API Demo", () => {
         // Get list of users
         const userData = await res.json();
         log("info", `List of users: ${JSON.stringify(userData)}`);
+
+        // Write the list of users
+        fileHelper.writeFile(`${process.cwd()}/data/api-res/list-of-users.json`, `${JSON.stringify(userData, undefined, 4)}`);
     });
 
     // POST Method
