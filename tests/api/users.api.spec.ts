73423a7da45a501b9d712604ebdfa0e89bca0c12
Periyanayagam Santhappan <copeautomation@gmail.com>
2025-10-06 13:03:20 +1100

    latest code

--- tests/api/users.api.spec.ts
+++ tests/api/users.api.spec.ts
@@ -1,32 +1,83 @@
-import { test, expect, request} from "@playwright/test"
-import { log } from "../helpers/logger.js"
+import { test, expect, request } from "@playwright/test";
+import { log } from "../helpers/logger.js";
 
 test.describe("REST API Demo", () => {
-    const baseURL = "https://reqres.in/api"
-
+    const baseURL = "https://reqres.in/api";
+    // GET Method
     test("Should get list of users", async ({ request }) => {
-    
         // Make a GET call
-        await log("info", `Making a GET call using ${baseURL}`)
+        await log("info", `Making a GET call using ${baseURL}`);
         const res = await request.get(`${baseURL}/users?page=2`, {
             headers: {
-                'x-api-key': 'reqres-free-v1'
-            }
-        })
+                "x-api-key": "reqres-free-v1",
+            },
+        });
 
         // Assert the status code
-        expect(res.status()).toBe(200)
-        await log("info", `The GET call is succesfull with ${res.status()}`)
+        expect(res.status()).toBe(200);
+        await log("info", `The GET call is succesfull with ${res.status()}`);
 
         // Get list of users
-        const userData = await res.json()
-        log("info", `List of users: ${JSON.stringify(userData)}`)
+        const userData = await res.json();
+        log("info", `List of users: ${JSON.stringify(userData)}`);
+    });
 
+    // POST Method
+    test("Should create a user", async ({ request }) => {
+        // Make a GET call
+        await log("info", `Making a POST call using ${baseURL}`);
+
+        const payload = {
+            name: "Alex",
+            job: "Thomas",
+            id: "125",
+            createdAt: "2025-10-06T01:35:49.877Z",
+        };
+
+        const res = await request.post(`${baseURL}/users`, {
+            headers: {
+                "x-api-key": "reqres-free-v1",
+                "Content-Type": "application/json",
+            },
+            data: payload,
+        });
+
+        // Assert the status code
+        expect(res.status()).toBe(201);
+        await log("info", `The POST call is succesfull with ${res.status()}`);
+
+        // Get list of users
+        const restData = await res.json();
+        log("info", `Response data from post call: ${JSON.stringify(restData)}`);
     });
 });
 
+/**
+ * POST Call
+ * var request = require('request');
+var options = {
+  'method': 'POST',
+  'url': 'https://reqres.in/api/users',
+  'headers': {
+    'x-api-key': 'reqres-free-v1',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    "name": "Alex",
+    "job": "Thomas",
+    "id": "124",
+    "createdAt": "2025-10-06T01:35:49.877Z"
+  })
+
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
+ */
 
 /**
+ * GET Call
  * var request = require('request');
 var options = {
   'method': 'GET',✅
